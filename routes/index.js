ea7e618d1f961214578ec8cbdfd4dc3981141abc
skeleborg <dark.anyra@gmail.com>
2015-06-24 16:22:12 +0200

    Creados créditos

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var quizController = require('quiz_controller');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Bienvenido a Quiz' });
+});
+
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
+
+router.get('/author', function(req, res) {
+	res.render('author', { title: 'Créditos', nombre: 'Patricia Fernández' });
 });
 
 module.exports = router;
