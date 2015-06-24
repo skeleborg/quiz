db4b0c2c6b2445b92088e18d3830819321cb9fc2
skeleborg <dark.anyra@gmail.com>
2015-06-24 16:49:09 +0200

    Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
-var quizController = require('quiz_controller');
 var router = express.Router();
+
+var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Bienvenido a Quiz' });
+  res.render('index', { title: 'Quiz' });
 });
 
 router.get('/quizes/question', quizController.question);
